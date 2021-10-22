5e5e46a313046e0428d83ab035445f461aae6423
prathweb-dev <83416384+prathweb-dev@users.noreply.github.com>
2021-10-22 10:53:37 +0530

    Add files via upload

---
+++ precache-manifest.97c9907f00fde0ed337af18d51f7a03b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ec49f4f08dd7cc645ff73a460b06a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a944a4a080d693e3877",
+    "url": "/static/css/main.9602a03d.chunk.css"
+  },
+  {
+    "revision": "eb23921373229e0998bd",
+    "url": "/static/js/2.c14ef57a.chunk.js"
+  },
+  {
+    "revision": "0ac0f02cf4dc2dce925fe7684e055e3a",
+    "url": "/static/js/2.c14ef57a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a944a4a080d693e3877",
+    "url": "/static/js/main.9b93a4f0.chunk.js"
+  },
+  {
+    "revision": "682f9dc0a88c8bde3947",
+    "url": "/static/js/runtime-main.eb0b6bae.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
